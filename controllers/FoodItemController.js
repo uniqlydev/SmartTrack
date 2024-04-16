6304a44bad9a208d51c0c30049321a7d4ad6d425
Brendan Castillo <bomber8184@icloud.com>
2024-04-16 19:30:56 +0800

    Added authentication

--- controllers/FoodItemController.js
+++ controllers/FoodItemController.js
@@ -1,16 +1,22 @@
 const { db, addDoc, collection } = require("../models/firebase");
 const FoodItemModel = require("../models/FoodItemModel");
+const { v4: uuidv4 } = require('uuid');
 
 
 exports.addFood = async (req, res) => {
     try {
         const { name, category, calories, date } = req.body;
+
+        // Generate a random UID for the food item
+        const uid = uuidv4();
+
     
         // Create a new FoodItemModel object
-        const foodItem = new FoodItemModel(null, name, category, calories, date);
+        const foodItem = new FoodItemModel(uid, name, category, calories, date);
 
         // Extract properties from the FoodItemModel instance
         const foodData = {
+            id: foodItem.id,
             name: foodItem.name,
             category: foodItem.category,
             calories: foodItem.calories,
@@ -21,8 +27,7 @@ exports.addFood = async (req, res) => {
         const foodCollection = collection(db, 'foods');
         const docRef = await addDoc(foodCollection, foodData);
 
-        
-
+    
         // Send the ID of the newly created document in the response
         res.status(200).send(docRef.id);
     } catch (error) {
